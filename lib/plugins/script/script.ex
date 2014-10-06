c05023bb1b56b40349e5954697c5f919962e9125
Taybin Rutkin <taybin@taybin.com>
2014-10-05 23:32:53 -0400

    add plugin form

--- lib/plugins/script/script.ex
+++ lib/plugins/script/script.ex
@@ -2,12 +2,27 @@ defmodule Gardenhose.Plugin.Script do
   use GenServer
   require Logger
 
-  def init(args) do
-    {:ok, []}
+  ##
+  # External
+  #
+  def start_link(stateFn) do
+    GenServer.start_link(__MODULE__, stateFn)
+  end
+
+  def start(pid) do
+    GenServer.cast(pid, {:start, :os.getpid()})
+  end
+
+  ##
+  # Internal
+  #
+
+  def init(stateFn) do
+    {:ok, stateFn.()}
   end
 
   def handle_cast({:start, parent_id}, state) do
-    Logger.warn("here we go")
-    {:noreply, [parent_id|state]}
+    Logger.info("here we go")
+    {:noreply, state}
   end
 end
