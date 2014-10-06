c006283144104a43be8ba14801de31948c728054
Taybin Rutkin <taybin@taybin.com>
2014-10-05 23:32:32 -0400

    remove registry agent

--- lib/plugins/plugin_sup.ex
+++ lib/plugins/plugin_sup.ex
@@ -1,7 +1,6 @@
 defmodule Gardenhose.Plugin.Supervisor do
   use Supervisor
 
-  @registry_name Gardenhose.Plugin.Registry
   @plugin_sup_name Gardenhose.Plugin.Supervisor
 
   def start_link do
@@ -10,7 +9,6 @@ defmodule Gardenhose.Plugin.Supervisor do
 
   def init([]) do
     children = [
-      worker(Gardenhose.Plugin.Registry, [[name: @registry_name]])
     ]
     supervise(children, strategy: :one_for_one)
   end
